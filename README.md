09d60a2ec144a6ef917a4084b352fb08012f3a01
Ivan Herman <ivan@ivan-herman.net>
2023-08-23 14:15:20 +0200

    Added some documentation

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# drawio-svg
-Temporarily handling some SVG export bugs  from draw.io
+# Process the SVG file exported from draw.io for VC WG related diagrams.
+
+The script is used to process the various SVG diagrams used in the vocabulary specification defined by the [W3C Verifiable Credential Working Group](https://www.w3.org/2017/vc/WG/). The reason for this script is a bug in draw.io in conjunction with links assigned to transparent shapes. The bug has been reported in a [github issue](https://github.com/jgraph/drawio/issues/3806); if the bug is taken care of in a later release of [draw.io](https://www.drawio.com/), then this script becomes unnecessary.
+
+Note however, that this script also includes an [SVGO](https://www.npmjs.com/package/svgo) processing; if the script itself becomes moot, SVGO should still be used. The main reason is to convert the SVG file into a real rescaled graphics, by removing the explicit width and height values from the top level `<svg>` element. Just for the records, here is the configuration of SVGO as used for the VC diagrams:
+
+```js
+{
+    plugins: [
+        {
+            name: 'preset-default',
+            params: {
+                overrides: {
+                    removeMetadata: false,
+                    removeTitle: false,
+                    removeDesc: false,
+                    convertShapeToPath: false,
+                    removeViewBox: false,
+                }
+            }
+        },
+        'removeDimensions'
+    ],
+    js2svg: {
+        indent: 4,
+        pretty: true
+    }
+}
+```
+
+This script, or SVGO, should be run on the diagram SVG files, as exported from [draw.io](https://www.drawio.com/), before incorporating them into the vocabulary definition files.
--- main.ts
+++ main.ts
@@ -1,8 +1,46 @@
+/**
+ * ## Process the SVG file exported from draw.io for VC WG related diagrams.
+ *
+ * The reason for this script is a bug in draw.io in conjunction with links assigned to transparent
+ * shapes. The bug has been reported in https://github.com/jgraph/drawio/issues/3806; if the bug
+ * is taken care of in a later release of draw.io, then this script becomes unnecessary.
+ *
+ * Note however, that this script also includes an [SVGO]() processing; if the script itself
+ * becomes moot, SVGO should still be used. The main reason is to convert the SVG file into
+ * a real rescaled graphics, by removing the explicit width and height values from the
+ * top level `<svg>` element.
+ *
+ * @param svg_text
+ * @param verbose
+ * @returns
+ */
+
 import { DOMParser, XMLSerializer } from 'xmldom';
 import { promises as fs }           from 'node:fs';
 import { Command }                  from 'commander';
 import * as svgo                    from 'svgo';
 
+/**
+ * Process the SVG file exported from draw.io for VC WG related diagrams. The draw.io
+ * error consists of, sometimes, omitting the `pointer-events="all"` attribute
+ * from shapes when they are otherwise transparent. That creates a problem for VC
+ * diagrams that assign a vocabulary URL to each shape referring to a term.
+ *
+ * The script localizes all `<a>` elements, takes the first element child (which is the enclosing shape)
+ * and adds the missing attribute.
+ *
+ * A minor issue is also taken care of: time to time draw.io adds an extra message to the bottom of the diagram
+ * which refers to a bug with text. That is also enclosed in an `<a>` element which is then removed from
+ * the SVG file.
+ *
+ * The function relies on the `xmldom` package for pure XML processing (the more widespread `jsdom` package
+ * cannot be used because it does not have an XML serializer). Note that I was cheating a bit with the types, referring
+ * to, e.g., `HTMLAnchorElement`; the reason is that I did not find the proper type hierarchy in `xmldom`...
+ *
+ * @param svg_text
+ * @param verbose
+ * @returns
+ */
 function parseAndProcess(svg_text: string, verbose: boolean = false): string {
     const svg = new DOMParser().parseFromString(svg_text);
     const document = svg.documentElement;
@@ -43,8 +81,13 @@ function parseAndProcess(svg_text: string, verbose: boolean = false): string {
     return (new XMLSerializer()).serializeToString(svg)
 }
 
-// parseAndProcess('test.svg','nami.svg');
-
+/**
+ * Main Entry point. Just call out to @link{parseAndProcess} and then run the resulting svg text
+ * through SVGO. See the [SVGO entry on npm](https://www.npmjs.com/package/svgo) for further details.
+ *
+ * Note the SVGO options used. If SVGO is used standalone in case this script becomes obsolete, those
+ * flags should be used...
+ */
 async function main() {
     const program = new Command();
     program
